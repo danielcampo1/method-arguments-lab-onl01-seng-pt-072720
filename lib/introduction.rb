cdb7a2d434ff41e4df658c91b47ff5ad75a5deb8
daniel campo <campo_daniel94@yahoo.com>
2020-07-29 04:44:36 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,10 +1,8 @@
 def introduction(name) 
   puts "my name is #{name}."
-  end
 end
 
 
 def introduction_with_language(name,language) 
   puts "hello, im #{name}, i am learning #{language}."
-  end 
 end
